8babe974316f338507d59a8c1e7cf26459123c1f
Daksh Sharma <daksh.sharma4295@gmail.com>
2026-08-10 18:04:05 +0530

    Update pbkdf2.cpp

--- pbkdf2.cpp
+++ pbkdf2.cpp
@@ -4,12 +4,8 @@
 
 using namespace std;
 
-// Derive a 256-bit AES key from the user's password using PBKDF2
-
 void derivekey(const std::string& password, const unsigned char salt[16], unsigned char key[32]) 
 {
- // Set the number of PBKDF2 iterations
-
     const int iterations = 100000;
 
     int result = PKCS5_PBKDF2_HMAC(password.c_str(), password.length(), salt, 16, iterations, EVP_sha256(), 32, key);
